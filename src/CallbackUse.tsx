7c6422014906b489bb26b2cbca095a692bec610a
Rohit <rohitgulati8@gmail.com>
2022-07-20 15:49:01 +0530

    commit

--- src/CallbackUse.tsx
+++ src/CallbackUse.tsx
@@ -1,5 +1,5 @@
 import Axios, {AxiosError, AxiosResponse} from "axios";
-import {useCallback, useContext, useMemo, useState} from "react";
+import {useCallback, useContext, useMemo, useRef, useState} from "react";
 import {AppContext} from "./App";
 import UserInfo from "./models/UserInfo";
 
@@ -7,7 +7,7 @@ const CallbackUse = () => {
     //userId can be used as a Prod here // mostly it will be impact of state change due to user Input
 //const [userId, setUserId] = useState("");
     const appContext = useContext(AppContext);
-
+const count = useRef(0);
     const [user, setUser] = useState({} as UserInfo);
     //creating a useCallback -- for using the function .. this function will not change unless userId changes
     const fetchUser = useCallback(
@@ -22,9 +22,10 @@ const CallbackUse = () => {
 
     useMemo(() => {
         fetchUser().then((res: UserInfo)=> setUser(res));
+        count.current = count.current+1;
     }, [fetchUser]);
 
-    return <div>{`${user.name}`}</div>
+    return <div>{`${user.name}     with count of render ${count.current}`}</div>
 }
 
 export default CallbackUse;
